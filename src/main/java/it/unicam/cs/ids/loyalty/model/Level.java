b6d2649ac8aa5f8fa5f635fc1a8fb089bb2e88c2
StefanoCruciani <stefano.cruciani@studenti.unicam.it>
2023-11-20 23:26:44 +0100

    Aggiunti repository, service e controller di "Level"

--- src/main/java/it/unicam/cs/ids/loyalty/model/Level.java
+++ src/main/java/it/unicam/cs/ids/loyalty/model/Level.java
@@ -1,12 +1,35 @@
 package it.unicam.cs.ids.loyalty.model;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+ 
+/**
+* Represents a level in a loyalty program.
+*/
+@Entity
 public class Level {
+ 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int uniqueId;
+ 
     private String name;
     private String description;
  
-    public Level(int uniqueId, String name, String description) {
-        this.uniqueId = uniqueId;
+    /**
+     * Default constructor.
+     */
+    public Level() {
+    }
+ 
+    /**
+     * Constructs a Level with the specified details.
+     *
+     * @param name        The name of the level.
+     * @param description The description of the level.
+     */
+    public Level(String name, String description) {
         this.name = name;
         this.description = description;
     }
@@ -15,12 +38,23 @@ public class Level {
         return uniqueId;
     }
  
+    public void setUniqueId(int uniqueId) {
+        this.uniqueId = uniqueId;
+    }
+ 
     public String getName() {
         return name;
+    }
+ 
+    public void setName(String name) {
+        this.name = name;
     }
  
     public String getDescription() {
         return description;
     }
-}
  
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
